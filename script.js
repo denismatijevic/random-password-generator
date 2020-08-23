e7f44d88c5fcf321e6b1c1064bc77e50eb56aa5f
denis matijevic <denisone90@gmail.com>
2020-08-22 21:13:43 -0600

    password is working

--- script.js
+++ script.js
@@ -1,60 +1,129 @@
-// Assignment Code
+// declaring variables
 var generateBtn = document.querySelector("#generate");
+var characters;
 
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword().innterHtml;
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
- // Password character options
- var lower = "abcdefghijklmnopqrstuvwxyz";
- var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
- var number = "1234567890";
- var symbol = "`~!@#$%^&*()-_=+[]{}\|?/.,><";
- var generatePassword = "";
- 
-function generatePassword(lower, upper, number, symbol) {
 
+function getPasswordChoices() {
+  var passwordLength = prompt ("Generate a password between 8 and 128 characters?");
+  if (passwordLength > 7 && (passwordLength < 129)) {
+  } else {alert ("Please choose a password length between 8 and 128 characters");
+ return; 
+ }; 
+  
+  
+  // Uppder case prompt
+  var passwordUpper = confirm ("Would you like upper case letters in your password?");
+ 
+  // Lower case prompt
+  var passwordLower = confirm ("Would you like lower case letters in your password?");
+ 
+  // Number prompt
+  var passwordNumber = confirm ("Would you like numbers in your password?");
+  
+  // Special character prompt
+  var passwordSymbol = confirm ("Now make it super strong by adding some special characters.")
+ 
+ if(!passwordUpper && !passwordLower && !passwordNumber && !passwordSymbol){
+   confirm ("please select one of the catagories");
+   return;
+ };
+ var passwordOptions = {
+   passwordLength: passwordLength,
+   passwordUpper: passwordUpper,
+   passwordLower: passwordLower,
+   passwordNumber: passwordNumber,
+   passwordSymbol: passwordSymbol
+ }
+ return passwordOptions;
 }
 
- // Password length
- var passwordLength = parseInt(prompt("How long would you like your password to be?"));
- console.log(passwordLength)
- if (passwordLength > 7 && (passwordLength < 129)) {
- } else {alert ("Please choose a password length between 8 and 128 characters");
- };
- 
- // Uppder case prompt
- var passwordUpper = confirm ("Would you like upper case letters in your password?");
- if (passwordUpper) {
- };
- 
- // Lower case prompt
- var passwordLower = confirm ("Would you like lower case letters in your password?");
- if (passwordUpper){
- };
- 
- // Number prompt
- var passwordNumber = confirm ("Would you like numbers in your password?");
- if (passwordNumber) {
- };
- 
- // Special character prompt
- var passwordSymbol = confirm ("Now make it super strong by adding some special characters.")
- if (passwordSymbol) {
- };
+ // We nned to make arrays of character options:
+function generatePassword() {
+let lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+let number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+let symbol = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+']
+  
+var possibleCharacters = []
+var result = ""
 
-//  for (var i = 0; i < passwordLength; i++) { 
-//   password = generatePassword + lower.charAt + upper.charAt + number.charAt + symbol.charAt(Math.floor(Math.random() * Math.floor(value.length -1)));
-//  }
-   
+var options = getPasswordChoices()
 
- function generatePassword() {
-   console.log("run")
- }
+if (options.passwordUpper){
+  possibleCharacters.push(upper)
+}
+if(options.passwordLower){
+  possibleCharacters.push(lower)
+}
+if(options.passwordNumber){
+  possibleCharacters.push(number)
+}
+if(options.passwordSymbol){
+  possibleCharacters.push(symbol)
+}
+if (!possibleCharacters){
+  alert("please pick one")
+  return; 
+}
+console.log(options.passwordLength)
+// for (var i = 0; i < options.passwordLength; i++) {
+var result = getRandomNumber(possibleCharacters, options.passwordLength)
+// result = result + newCharacter;
+// }
+appendHtml(result)
+// 1. We need to amke array's
+// 2. Based on user prompt we need to get random varibles 
+// 3. REMEBER NUMBER they select
+// 4. Push random varibles into our password string on line 3
+
+
+// Write password to the #password input
+
+
+
+  // for (i = 0; i <= passwordLength; i++) {
+  //   passwordLength = password.length;
+  //   randomCharacter = Math.floor(Math.random() * passwordLength)
+  //   password = password + passwordLength[randomCharacter]
+  // }
+  //   return password;
+}
+
+function getRandomNumber(arr, passwordLength) {
+  var passwordResult = ""
+  arr = shuffle(arr)
+  console.log(arr)
+  while(passwordResult.length < passwordLength) {
+    for (var i = 0; i < arr.length; i++){
+      var randomNumber = Math.floor(Math.random()* arr[i].length)
+      var randomElement = arr[i][randomNumber]
+      passwordResult = passwordResult + randomElement;
+    }
+  } 
+  console.log(passwordResult)
+  return passwordResult;
+}
+function shuffle(arr) {
+  var j, x, i;
+  for (i = arr.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = arr[i];
+      arr[i] = arr[j];
+      arr[j] = x;
+  }
+  return arr;
+}
+function appendHtml(password) {
+  console.log(password)
+  document.querySelector("#password").value = password;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
